f6c6c21160e8fabd1a6577e7b9f60a943e11be41
Franco Bulgarelli <franco@zaubersoftware.com>
2011-03-15 17:58:00 -0300

    Adding some tests

--- mule-module-s3/src/main/java/org/mule/module/s3/simpleapi/SimpleAmazonS3AmazonDevKitImpl.java
+++ mule-module-s3/src/main/java/org/mule/module/s3/simpleapi/SimpleAmazonS3AmazonDevKitImpl.java
@@ -1,11 +1,18 @@
-/*
- * $Id$
- * --------------------------------------------------------------------------------------
+/**
+ * Mule S3 Cloud Connector
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.mule.module.s3.simpleapi;
@@ -21,6 +28,7 @@ import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.amazonaws.services.s3.model.StorageClass;
 
@@ -138,13 +146,22 @@ public class SimpleAmazonS3AmazonDevKitImpl implements SimpleAmazonS3
 
     // 4.1
     public String createObject(@NotNull ObjectId objectId,
-                               InputStream input,
-                               ObjectMetadata metadata,
+                               @NotNull InputStream input,
+                               @NotNull ObjectMetadata metadata,
                                CannedAccessControlList acl,
                                StorageClass storageClass)
         throws AmazonClientException, AmazonServiceException
     {
-        return s3.putObject(objectId.getBucketName(), objectId.getKey(), input, metadata).getVersionId();
+        Validate.notNull(input);
+        Validate.notNull(metadata);
+        PutObjectRequest request = new PutObjectRequest(objectId.getBucketName(), objectId.getKey(), input,
+            metadata);
+        request.setCannedAcl(acl);
+        if (storageClass != null)
+        {
+            request.setStorageClass(storageClass);
+        }
+        return s3.putObject(request).getVersionId();
     }
 
     public void deleteObject(@NotNull ObjectId objectId) throws AmazonClientException, AmazonServiceException
--- mule-module-s3/src/test/java/org/mule/module/s3/S3TestCase.java
+++ mule-module-s3/src/test/java/org/mule/module/s3/S3TestCase.java
@@ -1,3 +1,19 @@
+/**
+ * Mule S3 Cloud Connector
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.mule.module.s3;
 
@@ -11,19 +27,23 @@ import org.mule.module.s3.simpleapi.SimpleAmazonS3AmazonDevKitImpl;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.BucketPolicy;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.CopyObjectResult;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.StorageClass;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
+import org.apache.commons.io.input.NullInputStream;
 import org.junit.Before;
 import org.junit.Test;
 
 public class S3TestCase
 {
+    private static final String MY_BUCKET = "myBucket";
     private S3CloudConnector connector;
     private AmazonS3 client;
 
@@ -36,24 +56,24 @@ public class S3TestCase
     }
 
     @Test
-    public void testchangeObjectStorageClass() throws AmazonClientException, AmazonServiceException
+    public void changeObjectStorageClass() throws AmazonClientException, AmazonServiceException
     {
-        connector.changeObjectStorageClass("myBucket", "myObject", "Standard");
-        verify(client).changeObjectStorageClass("myBucket", "myObject", StorageClass.Standard);
+        connector.changeObjectStorageClass(MY_BUCKET, "myObject", "Standard");
+        verify(client).changeObjectStorageClass(MY_BUCKET, "myObject", StorageClass.Standard);
     }
 
     @Test
     public void createBucket()
     {
-        connector.createBucket("myBucket", "US", null);
-        verify(client).createBucket(refEq(new CreateBucketRequest("myBucket", "US")));
+        connector.createBucket(MY_BUCKET, "US", null);
+        verify(client).createBucket(refEq(new CreateBucketRequest(MY_BUCKET, "US")));
     }
 
     @Test
     public void createBucketWithAcl()
     {
-        connector.createBucket("myBucket", "US", "Private");
-        CreateBucketRequest request = new CreateBucketRequest("myBucket", "US");
+        connector.createBucket(MY_BUCKET, "US", "Private");
+        CreateBucketRequest request = new CreateBucketRequest(MY_BUCKET, "US");
         request.setCannedAcl(CannedAccessControlList.Private);
         verify(client).createBucket(refEq(request));
     }
@@ -63,20 +83,38 @@ public class S3TestCase
     {
         CopyObjectResult result = new CopyObjectResult();
         result.setVersionId("12");
-        when(client.copyObject(refEq(new CopyObjectRequest("myBucket", "myObject", "myBucket", "myObject2")))).thenReturn(
+        when(client.copyObject(refEq(new CopyObjectRequest(MY_BUCKET, "myObject", MY_BUCKET, "myObject2")))).thenReturn(
             result);
 
-        assertEquals("12", connector.copyObject("myBucket", "myObject", null, "myObject2", null, null));;
+        assertEquals("12", connector.copyObject(MY_BUCKET, "myObject", null, "myObject2", null, null));;
     }
 
     @Test
     public void copyObjectBucketWithACL()
     {
-        CopyObjectRequest request = new CopyObjectRequest("myBucket", "myObject", "myBucket2", "myObject2");
+        CopyObjectRequest request = new CopyObjectRequest(MY_BUCKET, "myObject", "myBucket2", "myObject2");
         request.setCannedAccessControlList(CannedAccessControlList.Private);
         when(client.copyObject(refEq(request))).thenReturn(new CopyObjectResult());
 
-        assertNull(connector.copyObject("myBucket", "myObject", "myBucket2", "myObject2", "Private", null));
+        assertNull(connector.copyObject(MY_BUCKET, "myObject", "myBucket2", "myObject2", "Private", null));
+    }
+
+    @Test
+    public void createObject()
+    {
+        when(
+            client.putObject(refEq(new PutObjectRequest(MY_BUCKET, "myObject", new NullInputStream(0),
+                new ObjectMetadata()), "metadata", "inputStream"))).thenReturn(new PutObjectResult());
+
+        assertNull(connector.createObject(MY_BUCKET, "myObject", "have a nice release", null, null, null));
+    }
+
+    @Test
+    public void getBucketPolicy()
+    {
+        BucketPolicy policy = new BucketPolicy();
+        when(client.getBucketPolicy(MY_BUCKET)).thenReturn(policy);
+        assertSame(policy, connector.getBucketPolicy(MY_BUCKET));
     }
 
 }
